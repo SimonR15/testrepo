03c2fa7fc3906064f215d05f496b1e8aad4fd835
BT_1N3_33 <sample@bths.edu>
2025-01-10 13:41:23 -0500

    added if statement and print statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("I Want a sleep");
         System.out.println("You should get some sleep");
         System.out.println("i was goint to (its Friday)");
+        String simon = "tired";
+        if (simon == "tired") {
+            System.out.println("When you go home sleep");
+        }
     }
 }
