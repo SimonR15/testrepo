ebd8fb402c26385f0a0775680801886abdb843f5
BT_1N3_33 <sample@bths.edu>
2025-01-10 13:44:38 -0500

    changed print statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I Want a sleep");
+        System.out.println("I want to sleep");
         System.out.println("You should get some sleep");
         System.out.println("i was goint to (its Friday)");
         String simon = "tired";
